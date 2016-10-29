d9563577301f91ea36484c08a175d5a4ceebabdc
Trangell <shahryar.tbiz@gmail.com>
2016-10-29 14:14:50 +0330

    add external xml

--- fasttelegram.php
+++ fasttelegram.php
@@ -24,15 +24,151 @@ class plgContentFasttelegram extends JPlugin
 	 *
 	 * @since   1.6
 	 */
-	public function onContentAfterSave($context, $article, $isNew)
+	public function __construct(& $subject, $config) {
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	function onContentPrepareData($context, $data) {
+	
+		if (is_object($data))
+		{ 
+			$articleId = isset($data->id) ? $data->id : 0;
+			
+			if ($articleId > 0) { 
+				// Load the profile data from the database.
+				$db = JFactory::getDbo();
+				$query = $db->getQuery(true);
+				$query->select('message, url');
+				$query->from($db->qn('#__telegram'));
+				$query->where($db->qn('article_id') . ' = ' . $db->q($articleId));
+				$db->setQuery($query);
+				$results = $db->loadAssoc();
+
+				// Check for a database error.
+				if ($db->getErrorNum()) {
+					$this->_subject->setError($db->getErrorMsg());
+					return false;
+				}
+				
+				// show data.
+				$data->rating = array();
+				$data->rating['message'] = $results['message'];
+				$data->rating['url'] = $results['url'];
+			}
+			
+		}
+
+		return true;
+	}
+
+	function onContentPrepareForm($form, $data)
 	{
-			// sed session - > id
+		
+		if (!($form instanceof JForm))
+		{
+			$this->_subject->setError('JERROR_NOT_A_FORM');
+			return false;
+		}
+		
+		if ( $form->getName() != "com_content.article" ) {
+			return true;
+		}
+		// Add the extra fields to the form.
+		// need a seperate directory for the installer not to consider the XML a package when "discovering"
+		JForm::addFormPath(dirname(__FILE__) . '/rating');
+		$form->loadFile('rating', false);
+
+		return true;
+	}
+
+	public function onContentAfterSave($context, $article, $isNew) // Article is passed by value for joomla 3.x
+	{
+		// sed session - > id
 			$articleid = $article->id;
 			$session = JFactory::getSession();
 			$session->set('successfullSavecontent', "{$articleid}");
-     
 
-	}// end func onContentAfterSave
+		if (!in_array($context , array('com_content.article'))) {
+			return true; 
+		}
+		
+		$jinput = JFactory::getApplication()->input;
+		$form = $jinput->post->get('jform', 'null', 'array');
+	
+		if (is_array($form)) {
+			$test['message'] = $form['rating']['message'];
+			$test['url'] = $form['rating']['url'];
+		}
+		else {
+			return true ;
+		}
+
+		$articleId	= $article->id; // get article id
+		$chek = $this::searchRating($articleId); // search for exsit record
+		$publish =0;
+		
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		
+		if (!isset($chek) && $chek == null ){	 // ========================= insert
+				$columns = array('message', 'url','published','article_id');
+				$values = array($db->q($test['message']),$db->q($test['url']),$db->q($publish) ,$db->q($articleId));
+				$query->insert($db->qn('#__telegram'));
+				$query->columns($db->qn($columns));
+				$query->values(implode(',', $values));
+				$db->setQuery($query);
+				$db->execute(); 
+				$query->clear(); 
+		}
+		else { // ========================= update
+				$fields = array(
+					$db->qn('message') . ' = ' . $db->q($test['message']),
+					$db->qn('url') . ' = ' . $db->q($test['url']),
+					$db->qn('published') . ' = ' . $db->q($publish)
+				);
+				$conditions = array($db->qn('article_id') . ' = '. $db->q($articleId));
+				$query->update($db->qn('#__telegram'));
+				$query->set($fields);
+				$query->where($conditions);		
+				$db->setQuery($query);
+				$db->execute(); 		
+				$query->clear(); 
+		}
+		
+		return true;
+	}
+
+	public function onContentAfterDelete($context, $article) {
+		if ($article->id) {
+			try
+			{	
+				$db = JFactory::getDbo();
+				$query = $db->getQuery(true);
+				$query->delete($db->qn('#__telegram'));
+				$query->where($db->qn('article_id') . ' = ' . $db->q($article->id));
+				$db->setQuery($query);	
+				$db->execute(); 
+			}
+			catch (RuntimeException $e) { 
+				echo $e->getMessage(); 
+			}
+		}
+		return true;
+	}
+
+	public function searchRating($articleId)	{ 
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('id');
+		$query->from('#__telegram');
+		$query->where('article_id =  ' . $db->quote($articleId));
+		$db->setQuery($query);
+		$result = $db->loadResult();
+
+		return $result;
+	}
 
 	function onAfterDispatch(){
 
@@ -94,35 +230,5 @@ class plgContentFasttelegram extends JPlugin
 	} // end func onAfterDispatch
 
 
-	function onContentPrepareForm($form, $data) {
-
-		if (!($form instanceof JForm))
-		{
-			$this->_subject->setError('JERROR_NOT_A_FORM');
-			return false;
-		}
-		//------------------load form from file-----------------------------
-		JForm::addFormPath(dirname(__FILE__) . '/fasttelegram');
-		$form->loadFile('fasttelegram', false);
-		//-----------------get data from form--------------------------------
-		foreach ($data as $field) {
-			if(gettype($field) == "array") {
-				foreach($field as $key => $value) {
-					if($key == "message") {
-						$message = $value;
-					}
-					if($key == "url") {
-						$url = $value;
-					}
-				}
-			}
-		}
-
-		if(isset($message)) {
-			$db = JFactory::getDbo();
-			$db->setQuery("INSERT INTO #__telegram SET message='" .$message."',article_id=" . $data->id . ",url='" . $url . "'");
-			$db->execute();
-		} // end if
-		return true;
-	}// end func onContentPrepareForm
+	
 }
